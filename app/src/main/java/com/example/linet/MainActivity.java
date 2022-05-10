506c3c1b52aa7e6bdfc735bd728d087e02e063f3
linet <linigamba18@gmail.com>
2022-05-10 13:28:23 +0300

    first commit

---
+++ app/src/main/java/com/example/linet/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.linet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        Intent toprofile = new Intent(this, profileActivity.class);
+        startActivity(toprofile);
+    }
+}
